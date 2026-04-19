ae7406909d697a460a62d005a3e2b628775f0735
yurshanlin <yurshan.lin@gmail.com>
2026-04-19 13:21:38 +0800

    Update words.js

--- words.js
+++ words.js
@@ -1,35 +1,137 @@
 const WORDS = {
-  level1: [
-    {en:"one", zh:"一"},
-    {en:"two", zh:"二"},
-    {en:"three", zh:"三"},
-    {en:"dog", zh:"狗"},
-    {en:"cat", zh:"貓"},
-    {en:"apple", zh:"蘋果"},
-    {en:"red", zh:"紅色"},
-    {en:"blue", zh:"藍色"}
-  ],
-  level2: [
-    {en:"happy", zh:"開心"},
-    {en:"sad", zh:"難過"},
-    {en:"teacher", zh:"老師"},
-    {en:"school", zh:"學校"},
-    {en:"banana", zh:"香蕉"},
-    {en:"car", zh:"汽車"}
-  ],
-  level3: [
-    {en:"elephant", zh:"大象"},
-    {en:"kangaroo", zh:"袋鼠"},
-    {en:"umbrella", zh:"雨傘"},
-    {en:"beautiful", zh:"美麗"}
-  ],
-  level4: [
-    {en:"because", zh:"因為"},
-    {en:"different", zh:"不同"},
-    {en:"important", zh:"重要"}
-  ],
-  level5: [
-    {en:"understand", zh:"理解"},
-    {en:"remember", zh:"記住"}
-  ]
+
+level1: [
+{en:"zero",zh:"零"},{en:"one",zh:"一"},{en:"two",zh:"二"},{en:"three",zh:"三"},
+{en:"four",zh:"四"},{en:"five",zh:"五"},{en:"six",zh:"六"},{en:"seven",zh:"七"},
+{en:"eight",zh:"八"},{en:"nine",zh:"九"},{en:"ten",zh:"十"},
+{en:"eleven",zh:"十一"},{en:"twelve",zh:"十二"},
+{en:"thirteen",zh:"十三"},{en:"fourteen",zh:"十四"},
+{en:"fifteen",zh:"十五"},{en:"sixteen",zh:"十六"},
+{en:"seventeen",zh:"十七"},{en:"eighteen",zh:"十八"},
+{en:"nineteen",zh:"十九"},{en:"twenty",zh:"二十"},
+{en:"number",zh:"數字"}
+],
+
+level2: [
+{en:"good",zh:"好的"},{en:"bad",zh:"壞的"},{en:"big",zh:"大的"},
+{en:"small",zh:"小的"},{en:"happy",zh:"快樂的"},{en:"sad",zh:"傷心的"},
+{en:"hot",zh:"熱的"},{en:"cold",zh:"冷的"},
+{en:"new",zh:"新的"},{en:"old",zh:"舊的"},
+{en:"long",zh:"長的"},{en:"short",zh:"短的"},
+{en:"tall",zh:"高的"},{en:"young",zh:"年輕的"},
+{en:"many",zh:"很多"},{en:"little",zh:"少的"},
+{en:"angry",zh:"生氣的"},{en:"fine",zh:"很好"},
+{en:"sorry",zh:"對不起"},{en:"welcome",zh:"歡迎"}
+],
+
+level3: [
+{en:"ball",zh:"球"},{en:"baseball",zh:"棒球"},
+{en:"basketball",zh:"籃球"},{en:"football",zh:"美式足球"},
+{en:"soccer",zh:"足球"},{en:"tennis",zh:"網球"},
+{en:"bear",zh:"熊"},{en:"bird",zh:"鳥"},
+{en:"cat",zh:"貓"},{en:"cow",zh:"牛"},
+{en:"dog",zh:"狗"},{en:"duck",zh:"鴨"},
+{en:"elephant",zh:"大象"},{en:"fish",zh:"魚"},
+{en:"frog",zh:"青蛙"},{en:"horse",zh:"馬"},
+{en:"kangaroo",zh:"袋鼠"},{en:"lion",zh:"獅子"},
+{en:"monkey",zh:"猴子"},{en:"mouse",zh:"老鼠"},
+{en:"panda",zh:"熊貓"},{en:"pig",zh:"豬"},
+{en:"rabbit",zh:"兔子"},{en:"sheep",zh:"羊"},
+{en:"snake",zh:"蛇"},{en:"tiger",zh:"老虎"}
+],
+
+level4: [
+{en:"arm",zh:"手臂"},{en:"ear",zh:"耳朵"},
+{en:"eye",zh:"眼睛"},{en:"hair",zh:"頭髮"},
+{en:"hand",zh:"手"},{en:"head",zh:"頭"},
+{en:"leg",zh:"腳"},{en:"mouth",zh:"嘴巴"},
+{en:"nose",zh:"鼻子"},
+{en:"bag",zh:"包包"},{en:"book",zh:"書"},
+{en:"chair",zh:"椅子"},{en:"clock",zh:"時鐘"},
+{en:"desk",zh:"書桌"},{en:"door",zh:"門"},
+{en:"pen",zh:"筆"},{en:"pencil",zh:"鉛筆"},
+{en:"ruler",zh:"尺"},{en:"table",zh:"桌子"},
+{en:"wall",zh:"牆"},{en:"window",zh:"窗戶"}
+],
+
+level5: [
+{en:"belt",zh:"皮帶"},{en:"blouse",zh:"女襯衫"},
+{en:"shirt",zh:"襯衫"},{en:"shoe",zh:"鞋子"},
+{en:"shorts",zh:"短褲"},{en:"skirt",zh:"裙子"},
+{en:"sock",zh:"襪子"},{en:"T-shirt",zh:"T恤"},
+{en:"umbrella",zh:"雨傘"},{en:"watch",zh:"手錶"},
+{en:"black",zh:"黑色"},{en:"blue",zh:"藍色"},
+{en:"brown",zh:"棕色"},{en:"color",zh:"顏色"},
+{en:"gray",zh:"灰色"},{en:"green",zh:"綠色"},
+{en:"orange",zh:"橘色"},{en:"pink",zh:"粉紅"},
+{en:"purple",zh:"紫色"},{en:"red",zh:"紅色"},
+{en:"white",zh:"白色"},{en:"yellow",zh:"黃色"}
+],
+
+level6: [
+{en:"apple",zh:"蘋果"},{en:"banana",zh:"香蕉"},
+{en:"cake",zh:"蛋糕"},{en:"candy",zh:"糖果"},
+{en:"cookie",zh:"餅乾"},{en:"egg",zh:"蛋"},
+{en:"grape",zh:"葡萄"},{en:"ice",zh:"冰"},
+{en:"ice cream",zh:"冰淇淋"},{en:"juice",zh:"果汁"},
+{en:"lemon",zh:"檸檬"},{en:"melon",zh:"瓜"},
+{en:"orange_fruit",zh:"橘子"},
+{en:"peach",zh:"桃子"},{en:"pear",zh:"梨子"},
+{en:"pineapple",zh:"鳳梨"}
+],
+
+level7: [
+{en:"bed",zh:"床"},{en:"bottle",zh:"瓶子"},
+{en:"box",zh:"盒子"},{en:"cup",zh:"杯子"},
+{en:"floor",zh:"地板"},{en:"glass",zh:"玻璃"},
+{en:"light",zh:"燈"},{en:"phone",zh:"電話"},
+{en:"telephone",zh:"電話"},{en:"TV",zh:"電視"},
+{en:"boy",zh:"男孩"},{en:"girl",zh:"女孩"},
+{en:"man",zh:"男人"},{en:"woman",zh:"女人"},
+{en:"dad",zh:"爸爸"},{en:"mom",zh:"媽媽"},
+{en:"king",zh:"國王"},{en:"queen",zh:"女王"},
+{en:"teacher",zh:"老師"}
+],
+
+level8: [
+{en:"at",zh:"在"},{en:"in",zh:"在裡面"},
+{en:"on",zh:"在上面"},{en:"under",zh:"在下面"},
+{en:"this",zh:"這個"},{en:"that",zh:"那個"},
+{en:"these",zh:"這些"},{en:"those",zh:"那些"},
+{en:"I",zh:"我"},{en:"you",zh:"你"},
+{en:"he",zh:"他"},{en:"she",zh:"她"},
+{en:"it",zh:"它"},{en:"we",zh:"我們"},
+{en:"they",zh:"他們"},
+{en:"my",zh:"我的"},{en:"your",zh:"你的"},
+{en:"his",zh:"他的"},{en:"her",zh:"她的"},
+{en:"our",zh:"我們的"},{en:"their",zh:"他們的"}
+],
+
+level9: [
+{en:"how",zh:"如何"},{en:"what",zh:"什麼"},
+{en:"where",zh:"哪裡"},
+{en:"bus stop",zh:"公車站"},
+{en:"home",zh:"家"},{en:"house",zh:"房子"},
+{en:"school",zh:"學校"},{en:"zoo",zh:"動物園"},
+{en:"bike",zh:"腳踏車"},{en:"boat",zh:"船"},
+{en:"bus",zh:"公車"},{en:"car",zh:"汽車"},
+{en:"taxi",zh:"計程車"},{en:"train",zh:"火車"},
+{en:"truck",zh:"卡車"},
+{en:"go",zh:"去"},{en:"come",zh:"來"},
+{en:"have",zh:"有"},{en:"like",zh:"喜歡"},
+{en:"look",zh:"看"},{en:"open",zh:"打開"},
+{en:"close",zh:"關閉"},{en:"play",zh:"玩"},
+{en:"see",zh:"看見"},{en:"sing",zh:"唱歌"},
+{en:"sit",zh:"坐"},{en:"stand",zh:"站"},
+{en:"stop",zh:"停止"},{en:"want",zh:"想要"},
+{en:"watch",zh:"觀看"},
+{en:"hello",zh:"你好"},{en:"hi",zh:"嗨"},
+{en:"bye",zh:"再見"},{en:"goodbye",zh:"再見"},
+{en:"thank you",zh:"謝謝"},
+{en:"please",zh:"請"},
+{en:"yes",zh:"是"},{en:"no",zh:"不是"},
+{en:"OK",zh:"好"},{en:"name",zh:"名字"},
+{en:"time",zh:"時間"}
+]
+
 };
